02efadedfcad061ad1b7c25a137694229ff634a3
wandersontx <wandersonh89@gmail.com>
2020-08-05 10:56:51 -0300

    Configurando CORS

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,12 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+    	//Restrige acesso de sites a API. Neste caso esta permitindo que seja acessado por todos os sites
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            //Caso a API seja de consulta, podemos excluir os metodos POST, PUT, Delete
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,15 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
 // Routes
 require __DIR__ . '/routes/produtos.php';
 
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
+});
